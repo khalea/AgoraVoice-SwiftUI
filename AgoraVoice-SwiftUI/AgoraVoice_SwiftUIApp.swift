5b795455c6449a5d7bd1b079e28865005b8de37a
Javier Heisecke <javier.heisecke@consultr.net>
2022-11-22 16:15:31 -0300

    all files are added to an xcode project

---
+++ AgoraVoice-SwiftUI/AgoraVoice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgoraVoice_SwiftUIApp.swift
+//  AgoraVoice-SwiftUI
+//
+//  Created by Javier Heisecke on 2022-11-22.
+//
+
+import SwiftUI
+
+@main
+struct AgoraVoice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
